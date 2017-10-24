2ddae00f434c01aaa412b0e58c2248b76e60043f
pperez <patodeath@gmail.com>
2017-10-24 17:44:33 -0300

    Se crean los inserts de los usuarios reales del sistema

--- PodCli/src/java/model/bd/Data.java
+++ PodCli/src/java/model/bd/Data.java
@@ -18,7 +18,7 @@ public class Data {
                 "localhost",
                 "podcli",//nombre BD
                 "root",
-                ""//Password
+                "123456"//Password
         );
 
     }
