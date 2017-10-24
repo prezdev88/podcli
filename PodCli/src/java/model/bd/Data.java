a8ae142a138c13666ae411aeae9a0d83ec36b28e
Matikosososo <matiasjsd@gmail.com>
2017-10-24 15:05:44 -0300

    (DATA) metodo getPerfil (pedido del profe) -Maty

--- PodCli/src/java/model/bd/Data.java
+++ PodCli/src/java/model/bd/Data.java
@@ -1,6 +1,9 @@
 package model.bd;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Data {
     private Conexion con;
@@ -13,7 +16,6 @@ public class Data {
                 "root",
                 ""//Password
         ); 
-        
         //Metodos:
         
             //1)Antecedentes personales
@@ -34,10 +36,35 @@ public class Data {
             //Buscar paciente para listar (por rut, nombre, apellido y más??)
             
             //Buscar Paciente (atencion Podologica)
+            
+            //listar/get ficha
         
+            
+            // get estado civil y get perfil
         
+    } 
+    
+    public void crearFicha(Paciente p, Ficha f){
+        //se crea la ficha a partir de "Registrar Paciente"
     }
     
+    public List<Perfil> getPerfiles() throws SQLException{
+        ResultSet rs;
+        String query = "SELECT * FROM perfil;";
+        rs = con.ejecutarSelect(query);
+        
+        List<Perfil> list = new ArrayList<>();
+        Perfil p;
+        
+        while(rs.next()){
+            p = new Perfil();
+            p.setId(rs.getInt(0));
+            p.setNombre(rs.getString(1));
+            list.add(p);
+        }
+        con.desconectar();
+        return list;
+    }
     
 }
 //Si alguno ve que falta algo, Digalo por wsp o en algun momento, non se callen nada Saludos
