c6216ab6ce8f7886b4b176892fe3983f7d5418b6
D-loboli <lobolinkinxxx@gmail.com>
2017-10-24 16:08:32 -0300

    buscar y listar ficha

--- PodCli/src/java/model/bd/Data.java
+++ PodCli/src/java/model/bd/Data.java
@@ -103,7 +103,125 @@ public class Data {
     }
 
     public void crearFicha(Ficha f) throws SQLException {
-        con.ejecutar("inserte into ficha value(null,'" + f.getFecha() + "','" + f.getPaciente() + "','" + f.getUsuario() + "','" + f.getHta() + "','" + f.getDm() + "','" + f.getTipoDiabetes() + "','" + f.getAniosEvolucion() + "','" + f.isMixto() + "','" + f.isControl() + "','" + f.getFarmacoterapia() + "','" + f.getOtros() + "','" + f.getAlteracion() + "','" + f.getHabitos() + "','" + f.getTalla() + "','" + f.getImc() + "','" + f.isAmputacion() + "','" + f.getUbiAmputacion() + "','" + f.getnCalzado() + "','" + f.isVarices() + "','" + f.isHeridas() + "','" + f.getUbiHeridas() + "','" + f.getTipoHerida() + "','" + f.isTratamiento() + "','" + f.isNevos() + "','" + f.getUbiNevos() + "','" + f.isMaculas() + "','" + f.getTipoMaculas() + "')");
+        con.ejecutar("inserte into ficha value(null,'NOW()',"
+                    + "'" + f.getPaciente() + "',"
+                    + "'" + f.getUsuario() + "',"
+                    + "'" + f.getHta() + "',"
+                    + "'" + f.getDm() + "',"
+                    + "'" + f.getTipoDiabetes() + "',"
+                    + "'" + f.getAniosEvolucion() + "',"
+                    + "'" + f.isMixto() + "',"
+                    + "'" + f.isControl() + "',"
+                    + "'" + f.getFarmacoterapia() + "',"
+                    + "'" + f.getOtros() + "',"
+                    + "'" + f.getAlteracion() + "',"
+                    + "'" + f.getHabitos() + "',"
+                    + "'" + f.getTalla() + "',"
+                    + "'" + f.getImc() + "',"
+                    + "'" + f.isAmputacion() + "',"
+                    + "'" + f.getUbiAmputacion() + "',"
+                    + "'" + f.getnCalzado() + "',"
+                    + "'" + f.isVarices() + "',"
+                    + "'" + f.isHeridas() + "',"
+                    + "'" + f.getUbiHeridas() + "',"
+                    + "'" + f.getTipoHerida() + "',"
+                    + "'" + f.isTratamiento() + "',"
+                    + "'" + f.isNevos() + "',"
+                    + "'" + f.getUbiNevos() + "',"
+                    + "'" + f.isMaculas() + "',"
+                    + "'" + f.getTipoMaculas() + "')");
+    }
+    
+     public List<Ficha> buscarFicha(String filtro) throws SQLException{
+        List<Ficha> lista = new ArrayList<Ficha>();
+        
+        ResultSet rs = con.ejecutarSelect("select * from ficha WHERE usuario Like = '"+filtro+"'");
+        
+        Ficha f;
+        
+        while (rs.next()) {
+            f = new Ficha();
+            
+            f.setId(rs.getInt(1));
+            f.setFecha(rs.getTimestamp(2));
+            f.setPaciente(rs.getInt(3));
+            f.setUsuario(rs.getInt(4));
+            f.setHta(rs.getInt(5));
+            f.setDm(rs.getInt(6));
+            f.setTipoDiabetes(rs.getInt(7));
+            f.setAniosEvolucion(rs.getInt(8));
+            f.setMixto(rs.getBoolean(9));
+            f.setControl(rs.getBoolean(10));
+            f.setFarmacoterapia(rs.getString(11));
+            f.setOtros(rs.getString(12));
+            f.setAlteracion(rs.getString(13));
+            f.setHabitos(rs.getString(14));
+            f.setTalla(rs.getFloat(15));
+            f.setImc(rs.getFloat(16));
+            f.setAmputacion(rs.getBoolean(17));
+            f.setUbiAmputacion(rs.getString(18));
+            f.setnCalzado(rs.getInt(19));
+            f.setVarices(rs.getBoolean(20));
+            f.setHeridas(rs.getBoolean(21));
+            f.setUbiHeridas(rs.getString(22));
+            f.setTipoHerida(rs.getString(23));
+            f.setTratamiento(rs.getBoolean(24));
+            f.setNevos(rs.getBoolean(25));
+            f.setUbiNevos(rs.getString(26));
+            f.setMaculas(rs.getBoolean(27));
+            f.setTipoMaculas(rs.getString(28));
+            
+            lista.add(f);
+        }
+        con.desconectar();
+
+        return lista;
+    }
+     
+     public List<Ficha> listarFicha(String filtro) throws SQLException{
+        List<Ficha> lista = new ArrayList<Ficha>();
+        
+        ResultSet rs = con.ejecutarSelect("SELECT * FROM ficha;");
+        
+        Ficha f;
+        
+        while (rs.next()) {
+            f = new Ficha();
+            
+            f.setId(rs.getInt(1));
+            f.setFecha(rs.getTimestamp(2));
+            f.setPaciente(rs.getInt(3));
+            f.setUsuario(rs.getInt(4));
+            f.setHta(rs.getInt(5));
+            f.setDm(rs.getInt(6));
+            f.setTipoDiabetes(rs.getInt(7));
+            f.setAniosEvolucion(rs.getInt(8));
+            f.setMixto(rs.getBoolean(9));
+            f.setControl(rs.getBoolean(10));
+            f.setFarmacoterapia(rs.getString(11));
+            f.setOtros(rs.getString(12));
+            f.setAlteracion(rs.getString(13));
+            f.setHabitos(rs.getString(14));
+            f.setTalla(rs.getFloat(15));
+            f.setImc(rs.getFloat(16));
+            f.setAmputacion(rs.getBoolean(17));
+            f.setUbiAmputacion(rs.getString(18));
+            f.setnCalzado(rs.getInt(19));
+            f.setVarices(rs.getBoolean(20));
+            f.setHeridas(rs.getBoolean(21));
+            f.setUbiHeridas(rs.getString(22));
+            f.setTipoHerida(rs.getString(23));
+            f.setTratamiento(rs.getBoolean(24));
+            f.setNevos(rs.getBoolean(25));
+            f.setUbiNevos(rs.getString(26));
+            f.setMaculas(rs.getBoolean(27));
+            f.setTipoMaculas(rs.getString(28));
+            
+            lista.add(f);
+        }
+        con.desconectar();
+
+        return lista;
     }
 //Metodos:
 
