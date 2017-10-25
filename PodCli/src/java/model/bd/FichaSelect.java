8b9c1f985a17f6646ceaf096ce7ecf5735f02d79
Pitahhh <naxhoo2.0@live.cl>
2017-10-25 14:56:06 -0300

    Se creo la clase FichaSelect

---
+++ PodCli/src/java/model/bd/FichaSelect.java
@@ -0,0 +1,321 @@
+package model.bd;
+
+public class FichaSelect {
+    private int id; //nro ficha
+    private String paciente;
+    private String sexo;
+    private String fecha;
+    private String domicilio;
+    private String rut;
+    private String estado_civil;
+    private String actividad;
+    private String fono;
+    private String fecha_ficha;
+    private String encargado;
+    private String hta;
+    private String dm;
+    private String tipoDiabetes;
+    private String anioEvolucion;
+    private Boolean pacienteMixto;
+    private Boolean control;
+    private String farmacoteraía;
+    private String otrasPatologicas;
+    private String alteracionesOrtopedicas;
+    private String habitos_nocivos;
+    private String talla; // metros
+    private String imc;
+    private Boolean amputacion;
+    private String ubicacion_Amputacion;
+    private String nro_Calzando;
+    private Boolean varices;
+    private Boolean heridas;
+    private String ubicacion_Heridas;
+    private String tipo_Heridas;
+    private Boolean tratamiento;
+    private Boolean nevos;
+    private String ubicacion_nevos;
+    private Boolean maculas;
+    private String tipo_Maculas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getEstado_civil() {
+        return estado_civil;
+    }
+
+    public void setEstado_civil(String estado_civil) {
+        this.estado_civil = estado_civil;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public String getFono() {
+        return fono;
+    }
+
+    public void setFono(String fono) {
+        this.fono = fono;
+    }
+
+    public String getFecha_ficha() {
+        return fecha_ficha;
+    }
+
+    public void setFecha_ficha(String fecha_ficha) {
+        this.fecha_ficha = fecha_ficha;
+    }
+
+    public String getEncargado() {
+        return encargado;
+    }
+
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+
+    public String getHta() {
+        return hta;
+    }
+
+    public void setHta(String hta) {
+        this.hta = hta;
+    }
+
+    public String getDm() {
+        return dm;
+    }
+
+    public void setDm(String dm) {
+        this.dm = dm;
+    }
+
+    public String getTipoDiabetes() {
+        return tipoDiabetes;
+    }
+
+    public void setTipoDiabetes(String tipoDiabetes) {
+        this.tipoDiabetes = tipoDiabetes;
+    }
+
+    public String getAnioEvolucion() {
+        return anioEvolucion;
+    }
+
+    public void setAnioEvolucion(String anioEvolucion) {
+        this.anioEvolucion = anioEvolucion;
+    }
+
+    public Boolean getPacienteMixto() {
+        return pacienteMixto;
+    }
+
+    public void setPacienteMixto(Boolean pacienteMixto) {
+        this.pacienteMixto = pacienteMixto;
+    }
+
+    public Boolean getControl() {
+        return control;
+    }
+
+    public void setControl(Boolean control) {
+        this.control = control;
+    }
+
+    public String getFarmacoteraía() {
+        return farmacoteraía;
+    }
+
+    public void setFarmacoteraía(String farmacoteraía) {
+        this.farmacoteraía = farmacoteraía;
+    }
+
+    public String getOtrasPatologicas() {
+        return otrasPatologicas;
+    }
+
+    public void setOtrasPatologicas(String otrasPatologicas) {
+        this.otrasPatologicas = otrasPatologicas;
+    }
+
+    public String getAlteracionesOrtopedicas() {
+        return alteracionesOrtopedicas;
+    }
+
+    public void setAlteracionesOrtopedicas(String alteracionesOrtopedicas) {
+        this.alteracionesOrtopedicas = alteracionesOrtopedicas;
+    }
+
+    public String getHabitos_nocivos() {
+        return habitos_nocivos;
+    }
+
+    public void setHabitos_nocivos(String habitos_nocivos) {
+        this.habitos_nocivos = habitos_nocivos;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String getImc() {
+        return imc;
+    }
+
+    public void setImc(String imc) {
+        this.imc = imc;
+    }
+
+    public Boolean getAmputacion() {
+        return amputacion;
+    }
+
+    public void setAmputacion(Boolean amputacion) {
+        this.amputacion = amputacion;
+    }
+
+    public String getUbicacion_Amputacion() {
+        return ubicacion_Amputacion;
+    }
+
+    public void setUbicacion_Amputacion(String ubicacion_Amputacion) {
+        this.ubicacion_Amputacion = ubicacion_Amputacion;
+    }
+
+    public String getNro_Calzando() {
+        return nro_Calzando;
+    }
+
+    public void setNro_Calzando(String nro_Calzando) {
+        this.nro_Calzando = nro_Calzando;
+    }
+
+    public Boolean getVarices() {
+        return varices;
+    }
+
+    public void setVarices(Boolean varices) {
+        this.varices = varices;
+    }
+
+    public Boolean getHeridas() {
+        return heridas;
+    }
+
+    public void setHeridas(Boolean heridas) {
+        this.heridas = heridas;
+    }
+
+    public String getUbicacion_Heridas() {
+        return ubicacion_Heridas;
+    }
+
+    public void setUbicacion_Heridas(String ubicacion_Heridas) {
+        this.ubicacion_Heridas = ubicacion_Heridas;
+    }
+
+    public String getTipo_Heridas() {
+        return tipo_Heridas;
+    }
+
+    public void setTipo_Heridas(String tipo_Heridas) {
+        this.tipo_Heridas = tipo_Heridas;
+    }
+
+    public Boolean getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(Boolean tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public Boolean getNevos() {
+        return nevos;
+    }
+
+    public void setNevos(Boolean nevos) {
+        this.nevos = nevos;
+    }
+
+    public String getUbicacion_nevos() {
+        return ubicacion_nevos;
+    }
+
+    public void setUbicacion_nevos(String ubicacion_nevos) {
+        this.ubicacion_nevos = ubicacion_nevos;
+    }
+
+    public Boolean getMaculas() {
+        return maculas;
+    }
+
+    public void setMaculas(Boolean maculas) {
+        this.maculas = maculas;
+    }
+
+    public String getTipo_Maculas() {
+        return tipo_Maculas;
+    }
+
+    public void setTipo_Maculas(String tipo_Maculas) {
+        this.tipo_Maculas = tipo_Maculas;
+    }
+    
+    
+}
