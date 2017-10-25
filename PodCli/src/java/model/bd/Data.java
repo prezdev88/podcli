5e537ca07694bc27801366d5a5b94b5a29c74712
patrWan <gon.camilo.p@gmail.com>
2017-10-25 14:06:45 -0300

    Se realiza la busqueda de pacientes en buscar pacientes.jsp

--- PodCli/src/java/model/bd/Data.java
+++ PodCli/src/java/model/bd/Data.java
@@ -18,7 +18,7 @@ public class Data {
                 "localhost",
                 "podcli",//nombre BD
                 "root",
-                "123456"//Password
+                ""//Password
         );
 
     }
